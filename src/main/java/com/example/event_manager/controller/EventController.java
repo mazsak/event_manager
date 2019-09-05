c3ce4d801d1ff5df9d138f2b8c59ac7b3a46807c
Mateusz Samluk <mazsak97@gmail.com>
2019-09-05 13:42:00 +0200

    modify index and create add list predefined

--- src/main/java/com/example/event_manager/controller/EventController.java
+++ src/main/java/com/example/event_manager/controller/EventController.java
@@ -1,10 +1,13 @@
 package com.example.event_manager.controller;
 
+import com.example.event_manager.model.Event;
 import com.example.event_manager.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class EventController {
@@ -12,8 +15,14 @@ public class EventController {
   @Autowired private EventService eventService;
 
   @GetMapping("/")
-  public String alla(Model model) {
+  public String all(Model model) {
+    List<Event> list = eventService.findAll();
     model.addAttribute("events", eventService.findAll());
     return "index";
+  }
+
+  @GetMapping("/event_add")
+  public String add(Model model){
+
   }
 }
--- src/main/java/com/example/event_manager/controller/PredefinedController.java
+++ src/main/java/com/example/event_manager/controller/PredefinedController.java
@@ -1,19 +1,48 @@
 package com.example.event_manager.controller;
 
+import com.example.event_manager.model.Task;
+import com.example.event_manager.model.ToDoPredefined;
 import com.example.event_manager.service.ToDoPredefinedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PredefinedController {
 
-  @Autowired private ToDoPredefinedService toDoPredefinedServie;
+  @Autowired private ToDoPredefinedService toDoPredefinedService;
 
   @GetMapping("/predefined")
-  public String alla(Model model) {
-    model.addAttribute("predefineds", toDoPredefinedServie.findAll());
+  public String all(Model model) {
+    model.addAttribute("predefineds", toDoPredefinedService.findAll());
     return "predefined_list";
+  }
+
+  @GetMapping("/predefined_add")
+  public String add(Model model) {
+    model.addAttribute("predefined", new ToDoPredefined());
+    return "predefined_add";
+  }
+
+  @PostMapping(value = "/predefined_add", params = "action=add")
+  public String add_element(Model model, @ModelAttribute ToDoPredefined predefined) {
+    predefined.getTasks().add(Task.builder().name("").build());
+    model.addAttribute("predefined", predefined);
+    return "predefined_add";
+  }
+
+  @PostMapping(value = "/predefined_add", params = "action=save")
+  public String save(Model model, @ModelAttribute ToDoPredefined predefined) {
+    for(int i = 0 ; i < predefined.getTasks().size(); i++){
+      if (predefined.getTasks().get(i).getName().isEmpty()){
+        predefined.getTasks().remove(i);
+      }
+    }
+    toDoPredefinedService.save(predefined);
+    model.addAttribute("predefineds", toDoPredefinedService.findAll());
+    return "redirect:/predefined";
   }
 }
